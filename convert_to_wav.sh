1ab2cb34d862b46135e1da53b944516c7e70ef09
Pete Cartwright <pete.cartwright@gmail.com>
2016-10-17 22:33:00 -0400

    ....

--- convert_to_wav.sh
+++ convert_to_wav.sh
@@ -3,7 +3,7 @@
 # convert every mp3 in this directory to 16-bit WAV files
 for f in $( ls downloads/*.mp3 ); do 
     echo transcoding $f
-    ffmpeg -i "$f" -acodec pcm_s16le -ac 1 -ar 16000 "downloads/${f%.mp3}.wav"; 
+    ffmpeg -i "$f" -acodec pcm_s16le -ac 1 -ar 16000 "${f%.mp3}.wav"; 
 done
 
 # move all of these wav files to the wav/ directory
