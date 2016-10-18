f35cf589f95f0d1e7b17ec7eeca2e0cbda0741a8
Pete Cartwright <pete.cartwright@gmail.com>
2016-10-17 22:31:48 -0400

    handle relative paths properly :(

--- convert_to_wav.sh
+++ convert_to_wav.sh
@@ -3,10 +3,10 @@
 # convert every mp3 in this directory to 16-bit WAV files
 for f in $( ls downloads/*.mp3 ); do 
     echo transcoding $f
-    ffmpeg -i "downloads/$f" -acodec pcm_s16le -ac 1 -ar 16000 "downloads/${f%.mp3}.wav"; 
+    ffmpeg -i "$f" -acodec pcm_s16le -ac 1 -ar 16000 "downloads/${f%.mp3}.wav"; 
 done
 
 # move all of these wav files to the wav/ directory
 for i in $( ls downloads/*.mp3  ); do
-    mv downloads/$i downloads/wav/
+    mv $i downloads/wav/
 done
